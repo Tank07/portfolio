52aca9126b72cd6c0479fdf753f9b23f15fee5a1
Tank07 <tanc_@hotmail.it>
2022-09-21 16:18:22 +0200

    Added Bootstrap 5

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <button 
+  class="btn btn-primary" 
+  data-bs-target="#collapseTarget" 
+  data-bs-toggle="collapse">
+  Bootstrap collapse
+</button>
+<div class="collapse py-2" id="collapseTarget">
+  This is the toggle-able content!
+</div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
